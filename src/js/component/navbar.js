15b56adda5375c5e29ffbc3988cfc6320684a7cb
VMTore <veronicamt85@gmail.com>
2024-06-10 21:10:39 +0200

    Primera estructura

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,16 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-dark mb-3">
+      <Link to="/">
+        <h1 className="navbar-brand mb-0 p-2 h1">My Contact List App</h1>
+      </Link>
+      <div className="ml-auto mx-1 p-1">
+        <Link to="/contactlist">
+          <button className="btn btn-primary mx-2">Contact List</button>
+        </Link>
+        <Link to="/contactview/">
+          {" "}
+          <button className="btn btn-primary">Contact</button>
+        </Link>
+      </div>
+    </nav>
+  );
 };
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,62 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
+  return {
+    store: {
+      demo: [
+        {
+          name: "name",
+          email: "email",
+          phone: "phone",
+          adress: "adress",
+          rrss: "rrss",
+        },
+        {
+          title: "SECOND",
+          background: "white",
+          initial: "white",
+        },
+      ],
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
+      loadContactList: () => {
+        /**
 					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+	  }*/
+        const consultContactList = async () => {
+          try {
+            const response = await fetch(
+              "https://playground.4geeks.com/contact/agendas?offset=0&limit=100",
+              {
+                method: "GET",
+                headers: { accept: "aplication/json" },
+              }
+            );
+            const data = await response.json();
+            console.log(data);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+      
+      changeColor: (index, color) => {
+        //get the store
+        const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+        //we have to loop the entire demo array to look for the respective index
+        //and change its color
+        const demo = store.demo.map((elm, i) => {
+          if (i === index) elm.background = color;
+          return elm;
+        });
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+        //reset the global store
+        setStore({ demo: demo });
+      },
+    },
+  };
 };
 
 export default getState;
